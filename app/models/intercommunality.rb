d2aad6fc593cb938dd9be4803bac394cd812fde4
silenda <ml.endale@gmail.com>
2017-04-24 12:36:48 +0200

    adds a method that returns commune names and codes in a hash

--- app/models/intercommunality.rb
+++ app/models/intercommunality.rb
@@ -4,4 +4,11 @@ class Intercommunality < ApplicationRecord
   validates :siren, :name, presence: true
   validates :siren, uniqueness: true, numericality: {only_integer: true}, length: {is: 9}
   validates :form, inclusion: {in: %w(ca cu cc met)}
+
+  def communes_hash
+    hash = Hash.new
+    communes.each {|commune| hash[commune.code_insee.to_s] = commune.name}
+    hash
+  end
+
 end
