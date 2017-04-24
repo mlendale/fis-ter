8a3f843b56de8a59218d3e89aed60bab30adb7d6
silenda <ml.endale@gmail.com>
2017-04-24 20:44:49 +0200

    fixes slug method in intercommunality model

--- app/models/intercommunality.rb
+++ app/models/intercommunality.rb
@@ -5,19 +5,18 @@ class Intercommunality < ApplicationRecord
   validates :form, inclusion: {in: %w(ca cu cc met)}
   validates :siren, uniqueness: true, numericality: {only_integer: true}, length: {is: 9}
 
+  before_save :slug
+
   def communes_hash
     hash = Hash.new
     communes.each {|commune| hash[commune.code_insee.to_s] = commune.name}
     hash
   end
 
-  def generate_slug
-    if read_attribute(:slug).nil?
+  def slug
+    if name and read_attribute(:slug).nil?
       write_attribute(:slug, ActiveSupport::Inflector.parameterize(name))
-      save!
     end
     read_attribute(:slug)
   end
-
-alias slug generate_slug
 end
